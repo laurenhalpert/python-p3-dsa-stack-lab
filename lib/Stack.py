4b3f9fa628e565c4a5c0d90d7f56c77d2500f045
Lauren <halpertln@gmail.com>
2023-05-12 13:03:46 -0600

    complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -8,10 +8,15 @@ class Stack:
         return len(self.items) == 0
 
     def push(self, item):
+        if len(self.items) == self.limit:
+            return 'error: list already full'
         self.items.append(item)
 
     def pop(self):
-        self.items.pop()
+        if self.isEmpty():
+            return None
+            
+        return self.items.pop()
 
     def peek(self):
         return self.items[-1]
@@ -20,7 +25,8 @@ class Stack:
         return len(self.items)
 
     def full(self):
-        return len(self.items) == self.limit
+        return len(self.items) >= self.limit
+        
         
 
     def search(self, target):
